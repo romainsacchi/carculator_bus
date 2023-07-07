9b2e1657103490576c80ea6bda3d1a4963f6a475
Randy Duodu <duodurandy19@gmail.com>
2023-07-07 09:22:28 +0000

    Updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,6 @@ html_theme_options = {
     "site_url": "https://carculator-bus.readthedocs.io",
     "repo_url": "https://github.com/romainsacchi/carculator_bus/",
     "repo_name": "romainsacchi/carculator_bus",
-    "repo_type": "github",
     "edit_uri": "blob/master/docs/",
     "globaltoc_collapse": True,
     "features": ["navigation.top", "search.share", "navigation.tracking", "toc.follow"],
